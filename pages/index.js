a0975d353c06135fa9ca2bad5b1c3d6bd124ad3d
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2022-04-20 13:01:23 +0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,42 @@
+import Head from 'next/head';
+import Image from 'next/image';
+
+// Components
+import Header from '../components/Header';
+import Nav from '../components/Nav';
+import Results from '../components/Results';
+import requests from '../utils/requests';
+
+export default function Home({ results }) {
+  return (
+    <div>
+      <Head>
+        <title>Hulu Clone</title>
+        <meta
+          name="description"
+          content="Watch TV shows and movies online. Stream TV episodes of Grey's Anatomy, This Is Us, Bob's Burgers, Brooklyn Nine-Nine, Empire, SNL, and popular movies on your favorite devices. Start your free trial now. Cancel anytime."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <Nav />
+      <Results results={results} />
+    </div>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      results: request.results,
+    },
+  };
+}
